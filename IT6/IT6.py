b18a184951a77d07772bd433a32abc6f3c50d331
michaelmccoll <mccollmichael@hotmail.com>
2021-03-26 12:10:18 +0000

    IT6 code added

---
+++ IT6/IT6.py
@@ -0,0 +1,10 @@
+# Create a dictionary
+pets = {"dog1":"Harvey","cat1":"Misty","cat2":"Rambo"}
+
+print(pets)
+
+#  A function that uses dictionary
+pets.update({"dog2":"Puppy"})
+
+#  The result of the function on dictionary
+print(pets)
