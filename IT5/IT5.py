022fd62c04a8453a55dc5b6f7fd38a84357d0e5c
michaelmccoll <mccollmichael@hotmail.com>
2021-03-26 11:34:01 +0000

    IT5 code added to repo

---
+++ IT5/IT5.py
@@ -0,0 +1,11 @@
+#  Create a list of numbers
+lotto_numbers = [23,2,44,12,27]
+
+# Print list to the console
+print(lotto_numbers)
+
+# Using the 'append' function to add another number to the list
+lotto_numbers.append(30)
+
+# Print the list again to the console to view the result of the function
+print(lotto_numbers)
