7bd2a39ea82ebbeb6aad7c711a3005e858060cde
michaelmccoll <mccollmichael@hotmail.com>
2021-03-26 13:43:18 +0000

    IT3 code added

---
+++ IT3/IT3.py
@@ -0,0 +1,9 @@
+# A list of food created, containing 4 elements
+foods = ["Bacon Roll","Sandwich","Soup","Noodles"]
+
+# A for loop created that loops through each element in the list
+for food in foods:
+    if food == "Soup":              # If the element equals "soup"
+        print("This is soup")       # Then it prints "This is soup"
+    else:                           # If the element doesn't equal "soup"
+        print("Food is not soup")   # Then it prints "Food is not soup"
