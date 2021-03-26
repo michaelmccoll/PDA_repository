44bbb0f2321e9d78c562e94ad00316ec39ade92c
michaelmccoll <mccollmichael@hotmail.com>
2021-03-26 13:54:18 +0000

    IT4 code added

---
+++ IT4/IT4.py
@@ -0,0 +1,8 @@
+# Created a list of numbersm assigned to variable called 'age'
+ages = [44,62,14,5,19]
+
+# A function that sorts the data
+ages.sort()
+
+# The result of the function running
+print(ages)
